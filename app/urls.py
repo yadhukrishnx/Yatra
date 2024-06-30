a08bee4fed967953efccfd50fefb2e997329ce49
yadhukrishnx <yadhukrishnx@gmail.com>
2024-06-30 15:05:36 +0530

    Front end completed

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    
     path('', views.base, name='base'),
- 
+    path('destinations/', views.destinations, name='destinations'),
+    path('detailed_destination/', views.detailed_destination, name='detailed_destination'),
+    path('add_destination/', views.add_destination, name='add_destination'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def base(request):
     return render(request, 'base.html')
+
+def destinations(request):
+    return render(request, 'destinations.html')
+
+def detailed_destination(request):
+    return render(request, 'detailed_destination.html')
+
+def add_destination(request):
+    return render(request, 'add_destination.html')
